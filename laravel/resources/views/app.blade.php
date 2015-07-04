bee5cffb26ca8b760e3e4418dda35149590906d3
Olof Nilsson <olof.nilsson@findwise.com>
2015-07-04 21:43:27 +0200

    Makes navbar configurable via model

--- laravel/resources/views/app.blade.php
+++ laravel/resources/views/app.blade.php
@@ -16,6 +16,7 @@
 	<script src="/js/libs/backbone-react-component.js"></script>
 	<script src="/js/libs/uikit.min.js"></script>
 
+	<script src="/js/nav.js" type="text/jsx"></script>
 	<script src="/js/keys.js" type="text/jsx"></script>
 	<script src="/js/app.js" type="text/jsx"></script>
 
